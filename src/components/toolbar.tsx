fa5c046061651b14c8656942aa9814403e89aa58
eugenioenko <eyakhnenko@underline.com>
2024-10-31 20:32:51 -0700

    feat: base toolbar component

---
+++ src/components/toolbar.tsx
@@ -0,0 +1,67 @@
+import { useAppState } from "@/stores/app.store";
+import {
+  IconBrush,
+  IconColorPicker,
+  IconHandStop,
+  IconNewSection,
+  IconPointer,
+  IconZoom,
+} from "@tabler/icons-react";
+import { ColorPicker } from "primereact/colorpicker";
+
+export const Toolbar = () => {
+  return (
+    <div className="flex flex-col items-center gap-4 pt-4">
+      <ToggleButton value="hand">
+        <IconHandStop />
+      </ToggleButton>
+      <ToggleButton value="pointer">
+        <IconPointer />
+      </ToggleButton>
+      <ToggleButton value="brush">
+        <IconBrush />
+      </ToggleButton>
+      <ToggleButton value="selection">
+        <IconNewSection />
+      </ToggleButton>
+      <ToggleButton value="zoom">
+        <IconZoom />
+      </ToggleButton>
+      <Separator />
+      <ToggleButton value="picker">
+        <IconColorPicker />
+      </ToggleButton>
+      <ColorPicker />
+      <ColorPicker />
+      <Separator />
+    </div>
+  );
+};
+
+const Separator = () => {
+  return (
+    <div className="px-1 w-full">
+      <div className="border-t border-muted w-full"></div>
+    </div>
+  );
+};
+
+interface ToggleButtonProps {
+  value?: string;
+  children?: React.ReactNode;
+}
+
+const ToggleButton = ({ value, children }: ToggleButtonProps) => {
+  const current = useAppState((state) => state.tool);
+  const setTool = useAppState((state) => state.setTool);
+
+  return (
+    <button
+      type="button"
+      className={`p-1 rounded ${current === value ? "bg-primary" : ""}`}
+      onClick={() => setTool(value)}
+    >
+      {children}
+    </button>
+  );
+};
